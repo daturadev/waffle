713f74a4668680870554178062412fa7367231f2
j <58583697+daturadev@users.noreply.github.com>
2023-04-04 20:32:37 +0000

    Added updated FILES
    Need img dir

--- lib/parser.py
+++ lib/parser.py
@@ -1,22 +1,23 @@
 import json
-import ijson
-
-file = "data.json"
 
 with open('data.json') as f:
     data = json.load(f)
     #Extract each item from the meta.view.columns list
-    objects = ijson.items(f, 'meta.view.columns.item') 
+    objects = json.items(f, 'meta.view.columns.item')
     columns = list(objects) #Convert generator to list
 
-headers = objects['headers']
-store = objects['store']
+### headers = objects['headers']
+### store = objects['store']
+### injection_strings = objects['injection_strings']
 
-spoof_headers = headers['spoof_headers']
-tete_variation = headers['tete_variation']
-tete_headers = headers['tete_headers']
-tecl_headers = headers['tecl_headers']
-clte_headers = headers['clte_headers']
+### spoof_headers = headers['spoof_headers']
+### tete_variation = headers['tete_variation']
+### tete_headers = headers['tete_headers']
+### tecl_headers = headers['tecl_headers']
+### clte_headers = headers['clte_headers']
+### METHOD_SQL = injection_strings['METHOD_SQL']
 
-valid = store['valid']
-invalid = store['invalid']
+### valid = store['valid']
+###invalid = store['invalid']
+
+print(METHOD_SQL)
